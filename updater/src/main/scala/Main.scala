9ef54715b2af9d2f30231bee1539a3d181ee3699
Jacopo <jacopo.riciputi@gmail.com>
2020-01-28 13:48:23 +0100

    Modified updater

--- updater/src/main/scala/Main.scala
+++ updater/src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 import com.corundumstudio.socketio.{Configuration, SocketIOServer}
 import controller.Updater
 import model.dao.{ClientRedis, UPDATER_CONSUMER_ID, UPDATER_GROUP}
+import model.logger.Log
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
@@ -11,12 +12,14 @@ object Main extends App {
 
   val host: String = Option(System.getenv("WS_HOST")).getOrElse("localhost")
   val port: Int = Option(System.getenv("PORT")).getOrElse("9092").toInt
+  Log.debug("Port -> " + port)
   config.setHostname(host)
   config.setPort(port)
 
   val server = new SocketIOServer(config)
 
   server.start()
+  Log.debug("WS Server is up...")
 
   Await.ready(Updater(server, UPDATER_GROUP, ClientRedis.getConsumerId(UPDATER_CONSUMER_ID)).start(), Duration.Inf)
 
